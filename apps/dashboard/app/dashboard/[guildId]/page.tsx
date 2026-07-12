85cde8a7165a68ad556f43a2fdf6b3b8d6b64201
iuriineves <iuriayolphineves@gmail.com>
2026-07-12 23:42:59 +0100

    fix(dashboard): statically generated dynamic routes

--- apps/dashboard/app/dashboard/[guildId]/page.tsx
+++ apps/dashboard/app/dashboard/[guildId]/page.tsx
@@ -1,5 +1,13 @@
 "use server";
 
+import { getBotGuilds } from "@/app/actions/discord";
+import { Guild } from "@/lib/types";
+
+export async function generateStaticParams() {
+  const guilds = await getBotGuilds();
+  return guilds.map((guild: Guild) => ({ guildId: guild.id }));
+}
+
 export default async function GuildDashboard() {
   return <div className="w-full"></div>;
 }
