65213e0cd867b217d4d6fa78cbd7e20d2e6cf955
iuriineves <iuriayolphineves@gmail.com>
2026-07-12 16:42:14 +0100

    fix: use ACTUAL correct database filepath

--- packages/db/src/db.ts
+++ packages/db/src/db.ts
@@ -3,7 +3,7 @@ import { dirname } from "node:path";
 import Database from "better-sqlite3";
 import type { Database as DB } from "better-sqlite3";
 
-const dbPath = "data/bot.db";
+const dbPath = "/app/data/bot.db";
 
 console.log(`Database path: ${dbPath}`);
 
